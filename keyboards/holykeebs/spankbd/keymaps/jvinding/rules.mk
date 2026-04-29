619c0754a2bcae74febe65f9eb7f66688c9edf40
Jeremy Vinding <github@jeremyvinding.com>
2026-04-29 10:34:13 -0600

    add span to my layouts

---
+++ keyboards/holykeebs/spankbd/keymaps/jvinding/rules.mk
@@ -0,0 +1,39 @@
+USER_NAME = jvinding
+
+# Right half is master (USB-connected side)
+OPT_DEFS += -DHK_MASTER_RIGHT
+OPT_DEFS += -DHK_POINTING_DEVICE_RIGHT_TRACKPOINT
+
+# Split serial (RP2040 PIO vendor driver)
+SERIAL_DRIVER = vendor
+
+# Trackpoint via PS/2
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = ps2
+PS2_ENABLE             = yes
+PS2_DRIVER             = vendor
+
+# OLED
+OLED_ENABLE       = yes
+OLED_DRIVER       = ssd1306
+
+# ps2_mouse.c provides ps2_mouse_init() used by drivers/sensors/ps2.c
+SRC += $(TOP_DIR)/drivers/ps2/ps2_mouse.c
+
+# Holykeebs trackpoint plumbing
+SRC += $(TOP_DIR)/users/holykeebs/holykeebs.c
+SRC += $(TOP_DIR)/users/holykeebs/trackpoint.c
+SRC += $(TOP_DIR)/users/holykeebs/pimoroni.c
+SRC += $(TOP_DIR)/users/holykeebs/hk_debug.c
+SRC += $(TOP_DIR)/users/holykeebs/rpc.c
+
+# jvinding user space OLED
+SRC += $(TOP_DIR)/users/jvinding/oled.c
+
+# Required by oled.c for caps word sync
+CAPS_WORD_ENABLE     = yes
+
+MOUSEKEY_ENABLE      = yes
+
+# keymap.c includes jvinding.h from the user space directory
+EXTRAINCDIRS += users/jvinding
