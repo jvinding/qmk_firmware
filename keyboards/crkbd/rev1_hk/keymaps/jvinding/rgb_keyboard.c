e83742f7db0351b5b4069cfdf891b35df04a2a72
Jeremy Vinding <github@jeremyvinding.com>
2026-05-06 07:16:30 -0600

    turn off the underglow leds in the hk crkbd since it's in an opaque case

--- keyboards/crkbd/rev1_hk/keymaps/jvinding/rgb_keyboard.c
+++ keyboards/crkbd/rev1_hk/keymaps/jvinding/rgb_keyboard.c
@@ -8,8 +8,12 @@
 // for any Corne-specific LEDs that need different treatment.
 void jv_rgb_matrix_indicators_keyboard(uint8_t active_layer, uint8_t led_min, uint8_t led_max) {
     (void)active_layer;
-    (void)led_min;
-    (void)led_max;
+    // Opaque case: underglow LEDs are not visible, turn them off.
+    for (uint8_t i = led_min; i < led_max; i++) {
+        if (HAS_FLAGS(g_led_config.flags[i], LED_FLAG_UNDERGLOW)) {
+            rgb_matrix_set_color(i, 0, 0, 0);
+        }
+    }
 }
 
 #endif
